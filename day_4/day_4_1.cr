74c6ef0a74bcf8cc68a15141d6323d2aab46c108
Kirk Haines <wyhaines@gmail.com>
2022-12-24 09:59:57 -0700

    Day 4_1 committed. Committing Day 4_2 as soon as I finish that part of the article writeup.

---
+++ day_4/day_4_1.cr
@@ -0,0 +1,41 @@
+class CampCleanup
+  def initialize(filename)
+    assignments = parse_assignments(filename)
+
+    puts "Of #{assignments.size} assignments, #{count_redundant_assignments(assignments)} are redundant."
+  end
+
+  def parse_assignments(filename)
+    File.read(filename).split("\n").reject(&.empty?).map do |line|
+      line.split(",").map do |assignment|
+        min, max = assignment.split("-").map(&.to_i)
+        (min..max)
+      end
+    end
+  end
+
+  def count_redundant_assignments(assignments)
+    assignments.select do |(left, right)|
+      redundant?(left, right)
+    end.size
+  end
+
+  def redundant?(left, right)
+    smaller, larger = sort_by_containment(left, right)
+    if smaller.min >= larger.min && smaller.max <= larger.max
+      true
+    else
+      false
+    end
+  end
+
+  def sort_by_containment(left, right)
+    if left.min <= right.min && left.max >= right.max
+      [right, left]
+    else
+      [left, right]
+    end
+  end
+end
+
+CampCleanup.new(ARGV[0]? || "input.txt")
---
+++ day_4/day_4_1.rs
@@ -0,0 +1,78 @@
+use std::env;
+use std::fs;
+
+struct CampCleanup {
+    assignments: Vec<Vec<Assignment>>,
+}
+
+#[derive(Debug)]
+struct Assignment {
+    start: i32,
+    end: i32,
+}
+
+impl CampCleanup {
+    fn new(filename: &str) -> CampCleanup {
+        let assignments = Self::parse_assignments(filename);
+
+        CampCleanup { assignments }
+    }
+
+    fn run(&self) {
+        println!(
+            "Of {} assignments, {} are redundant.",
+            self.assignments.len(),
+            self.count_redundant_assignments(&self.assignments)
+        );
+    }
+
+    fn parse_assignments(filename: &str) -> Vec<Vec<Assignment>> {
+        let text = std::fs::read_to_string(filename).unwrap();
+        text.lines()
+            .map(|line| {
+                line.split(",")
+                    .map(|assignment| {
+                        let mut parts = assignment.split("-");
+                        let min = parts.next().unwrap().parse().unwrap();
+                        let max = parts.next().unwrap().parse().unwrap();
+                        Assignment {
+                            start: min,
+                            end: max,
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>()
+    }
+
+    fn count_redundant_assignments(&self, assignments: &Vec<Vec<Assignment>>) -> usize {
+        (*assignments)
+            .iter()
+            .filter(|assignment| self.is_redundant(&assignment[0], &assignment[1]))
+            .count()
+    }
+
+    fn is_redundant(&self, left: &Assignment, right: &Assignment) -> bool {
+        let (smaller, larger) = self.sort_by_containment(left, right);
+        (*smaller).start >= (*larger).start
+            && (*smaller).end <= (*larger).end
+    }
+
+    fn sort_by_containment<'a>(&'a self, left: &'a Assignment, right: &'a Assignment) -> (&Assignment, &Assignment) {
+        if (*left).start <= (*right).start && (*left).end >= (*right).end {
+            (right, left)
+        } else {
+            (left, right)
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut filename = "input.txt";
+    if let Some(arg) = args.get(1) {
+        filename = arg;
+    }
+
+    CampCleanup::new(filename).run();
+}
